54e1dc06f09b004204d3580eee1e88c82e792122
ifcassianasl <cassianasilveira64@gmail.com>
2021-11-05 21:32:55 -0300

    Pagina de em construçao

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      mRed: '#B70001',
+      mYellow: '#FF8001',
+      mBlue: '#13017C',
+      mPink: '#F447A1'
+    },
+    animation: {
+      'spin-slow': 'spin 3.5s linear infinite',
+    }
+  },
+  variants: {
+    extend: {
+      animation: ['hover', 'motion-safe', 'responsive']
+    },
+  },
+  plugins: [],
+}
