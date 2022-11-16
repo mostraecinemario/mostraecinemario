618304eb35a6c254b4c575bb14489ffb9c260c7b
ifcassianasl <cassianasilveira64@gmail.com>
2022-11-16 13:48:12 -0300

    adicionar texto em exibições

--- src/pages/exibicoes/index.jsx
+++ src/pages/exibicoes/index.jsx
@@ -9,7 +9,7 @@ const CinemaItinerante = () => {
       </Head>
 
       <Text fontWeight="bold" alignSelf={"start"} px="20%" fontSize="4xl" textTransform="uppercase" mb={2}>Exibições</Text>
-      <Text px="15%">Para este ano, as exibições dos filmes irão acontecer em um formato itinerante com o CINEMA DE RUA. Iremos levar nosso cinema para diferentes cantos da cidade do Rio Grande. Nossa primeira exibição será no Rincão da Cebola, no Centro da cidade, depois iremos levar nosso cinema até a Orla da Henrique Pancada, seguindo para a Ilha dos Marinheiros e logo para o Taim, encerrando nosso evento no Cassino. Cada exibição irá acontecer em um dia da semana, de segunda a sexta, às 19h. Acompanhe pelo mapa onde será cada exibição para saber qual lugar fica mais próximo de você.</Text>
+      <Text px="15%" className="justify">Para este ano, as exibições dos filmes irão acontecer em um formato itinerante com o CINEMA DE RUA. Iremos levar nosso cinema para diferentes cantos da cidade do Rio Grande. Nossa primeira exibição será no Rincão da Cebola, no Centro da cidade, depois iremos levar nosso cinema até a Orla da Henrique Pancada, seguindo para a Ilha dos Marinheiros e logo para o Taim, encerrando nosso evento no Cassino. Cada exibição irá acontecer em um dia da semana, de segunda a sexta, às 19h. Acompanhe pelo mapa onde será cada exibição para saber qual lugar fica mais próximo de você.</Text>
       <Stack flex='.8' align={"center"}>
         <AspectRatio w="80%" ratio={{base: 1 / 2, md: 16 / 9}}>
           <iframe src="https://www.google.com/maps/d/embed?mid=1MNCoheTDHkGObLQOq5cOFqn7qv9ePE0&ehbc=2E312F"></iframe>
